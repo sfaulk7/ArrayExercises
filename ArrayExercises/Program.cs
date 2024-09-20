761b28faeb94fda899f5911ff4fbc072cc06ed66
sfaulk7 <sfaulk.busy@gmail.com>
2024-09-20 10:51:35 -0500

    Checking to see if repo works

---
+++ ArrayExercises/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrayExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //l
+        }
+    }
+}
